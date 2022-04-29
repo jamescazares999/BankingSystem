5d1cdf59bd9601dda455ef7537849a98c6411aa5
sirp140 <85513412+sirp140@users.noreply.github.com>
2022-04-28 19:55:15 -0700

    Create ATM.java

---
+++ Code/ATM.java
@@ -0,0 +1,18 @@
+public class Atm extends BankSystem {
+	
+
+	
+	Atm(int idNum, int cardNum) {
+		super(idNum, cardNum);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void printReceipt() {
+		
+		
+		
+		
+		
+	}
+
+}
