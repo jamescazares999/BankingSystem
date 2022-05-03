3576e20b7af21f81caa06d9b10444ac2d0534fc7
Khalil Cato <98295027+ProtoRed@users.noreply.github.com>
2022-05-02 21:00:48 -0700

    Update ATM_interface.java
    updated

--- Code/ATM_interface.java
+++ Code/ATM_interface.java
@@ -4,12 +4,6 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 import javax.swing.*;
 import java.util.Scanner;
 import java.io.File;
@@ -34,29 +28,41 @@ public class ATM_interface  {
 		
 		private static void loginUI(final JFrame frame2) {
 			JTextField textField = new JTextField(30);
+			JTextField textField2 = new JTextField(30);
+			//JPasswordField passwordField = new JPasswordField();
 			JPanel panel2 = new JPanel();
 			JLabel pinLabel = new JLabel("please insert pin");
 			pinLabel.setBounds(150, 220, 150, 30);
 			JButton loginButton = new JButton("submit");
 			JButton goBack = new JButton("going back to the previous page");
 			
-			
+			//Login button 
 			loginButton.addActionListener(new ActionListener() {
 		        public void actionPerformed(ActionEvent e) {
 		        String pinCheck;
+		        String debCheck;
 		        pinCheck = textField.getText();
-		        String pin;
-		        /*---------------Text file goes in here---------------------------*/
-		        File text = new File("C:\\Users\\proto\\eclipse-workspace\\ATM\\src\\testpin.txt");
-		        /*------------------------------------------------------------------*/
+		        debCheck = textField2.getText();
+		        //String pin;
 		        
+		        /*---------------Text files goes in here------------------------------------------*/
+		        File text = new File("C:\\Users\\proto\\eclipse-workspace\\ATM\\src\\testpin.txt"); //used to open file which contains all pin numbers
+		        File text2 = new File("C:\\Users\\proto\\OneDrive\\Documents\\debitCard.txt"); //used to open credit card text file
+		        /*--------------------------------------------------------------------------------*/
+		      
+		        //------------reads through the file to check the pin and card number------------------------//
 		        try {
 					Scanner myRead = new Scanner(text);
-					while (myRead.hasNextLine()) {
+					Scanner myRead2 = new Scanner(text2);
+					while (myRead.hasNextLine()&& myRead2.hasNextLine()) {
 						String data = myRead.nextLine();
+						String data2 = myRead2.nextLine();
 						goBack.setEnabled(true);//used to return back to the previous page
 				           //to check if the pin entered is correct or not
-				           if (pinCheck.equalsIgnoreCase(data)){
+				           if (debCheck.equalsIgnoreCase(data)&& pinCheck.equalsIgnoreCase(data2)){
+				        	   //passwordField.setEchoChar('*');
+				        	   textField.setVisible(false);
+				        	   textField2.setVisible(false);
 				                JOptionPane.showMessageDialog(frame2, "Login Successful");
 				            atmWindow();// leads to the atm gui window
 				           } 
@@ -71,6 +77,7 @@ public class ATM_interface  {
 					e1.printStackTrace();
 				}
 		        
+		 
 		        //JOptionPane.showMessageDialog(frame2, "text");
 		        /*   
 		        goBack.setEnabled(true);//used to return back to the previous page
@@ -91,6 +98,7 @@ public class ATM_interface  {
 			//adding the panels to the GUI
 			panel2.add(pinLabel);
 			panel2.add(textField);
+			panel2.add(textField2);
 			panel2.add(loginButton);
 			frame2.getContentPane().add(panel2);
 			
@@ -107,7 +115,7 @@ public class ATM_interface  {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		
 		atmUI(frame); // setting frame
-		frame.setSize(600,600); //setting the frame size for the gui
+		frame.setSize(400,400); //setting the frame size for the gui
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);  //  to Center UI on screen
 		
@@ -117,11 +125,9 @@ public class ATM_interface  {
 	//contains the main buttons
 	private static void atmUI(final JFrame frame) {
 	JPanel panel = new JPanel();
+	File text = new File("C:\\Users\\proto\\eclipse-workspace\\ATM\\src\\testpin.txt");
 	//LayoutManager layout = new GridLayout();  // uncomment: To try alternate layouts
     //panel.setLayout(layout);  
-	//JLabel user_label, password_label;
-	//user_label = new JLabel();
-	//user_label.setText("please input your PIN");
 	
 	
 	//password_label = new JLabel();
@@ -138,7 +144,7 @@ public class ATM_interface  {
    
     //withdraw savings
     withsavingButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
+    	public void actionPerformed(ActionEvent e) {
            JOptionPane.showMessageDialog(frame, "test");
            goBack.setEnabled(true);//used to return back to the previous page
         }
