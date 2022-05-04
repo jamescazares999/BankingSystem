6aced4a7dc0824c5bdab6890f8c55f343c3b8715
James C <71861391+jamescazares999@users.noreply.github.com>
2022-05-03 21:28:28 -0700

    Update Account.java

--- Code/Account.java
+++ Code/Account.java
@@ -1,23 +1,22 @@
-
 public class Account {
-private int linkedMember;
-private double balance;
+  private int linkedMember;
+  private double balance;
 
- Account (int member){
-	this.linkedMember = member;
-	this.balance =0;
-}
-public int getLinkedMember() {
-	return linkedMember;
-}
-public void setLinkedMember(int linkedMember) {
-	this.linkedMember = linkedMember;
-}
-public double getBalance() {
-	return balance;
-}
-public void setBalance(double balance) {
-	this.balance = balance;
-}
+  Account(int member) {
+    this.linkedMember = member;
+    this.balance = 0;
+  }
+  public int getLinkedMember() {
+    return linkedMember;
+  }
+  public void setLinkedMember(int linkedMember) {
+    this.linkedMember = linkedMember;
+  }
+  public double getBalance() {
+    return balance;
+  }
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
 
 }
